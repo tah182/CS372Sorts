5909b2eb24a4420791f07b1c4db7250142d33eb2
faust06 <faust06@me.com>
2015-04-02 19:39:52 -0700

    Added functionideas.txt

---
+++ Assn4_Sorts/Assn4_Sorts/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assn4_Sorts
+//
+//  Created by nrtownsend on 4/2/15.
+//  Copyright (c) 2015 nrtownsend. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
