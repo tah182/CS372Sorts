7b59395ffc54f17f27d09a846ba59b3714d1b896
faust06 <faust06@me.com>
2015-04-11 11:33:02 -0700

    added main()

--- Assn4_Sorts/Assn4_Sorts/main.cpp
+++ Assn4_Sorts/Assn4_Sorts/main.cpp
@@ -9,7 +9,18 @@
 #include "Common.h"
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    int optionArray[MENU_CHOICE_NUMS];              //array to hold user's menu selections
+   
+    //seeds random number generator with current time
+    srandom(time(NULL));
+    
+    //displays main menu to user and reads user choice of sorting algorithms and number of tests
+    displayMainMenu(optionArray);
+    
+    //performs tests for chosen sorting algorithms and displays results to user
+    loopControl(optionArray);
+    
+    //end of program
     return 0;
 }
