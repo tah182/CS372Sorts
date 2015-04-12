48286b2cfe03d2ece642faa1c8d20ea776ca8886
faust06 <faust06@me.com>
2015-04-12 16:50:37 -0700

    added file header to main()

--- Assn4_Sorts/Assn4_Sorts/main.cpp
+++ Assn4_Sorts/Assn4_Sorts/main.cpp
@@ -1,10 +1,36 @@
-//
-//  main.cpp
-//  Assn4_Sorts
-//
-//  Created by nrtownsend on 4/2/15.
-//  Copyright (c) 2015 nrtownsend. All rights reserved.
-//
+//*********************************************************************
+// CODE FILENAME:   townsendTatsumoto-assn4-prog.cpp
+// DESCRIPTION:     Program analyzes efficiency of sorting alogrithms.
+//                  Menu is display in which user can choose two sorting algorithms and a number of tests over
+//                  which to test the algorithms. After these options are chosen, the program sorts dynamically
+//                  allocated arrays using the chosen methods and the length of time taken to perform the sort as well
+//                  as the average time for each chosen sort is displayed to the user. The user can then choose to
+//                  run the program again or exit
+//                  or exit the program.
+// CLASS/TERM:      2015 Spring 8 week Section 2
+// DESIGNER:        Neil Townsend and Tah Tatsumoto
+// FUNCTIONS        generateArray       generates a dynamically allocated array of variable size
+//                  fillRandomArray     fills a given array with random values between ranges
+//                  getRandInt          generates a random number within given ranges
+//                  getAvgTime          calculates the average value of an array of times
+//                  insertionSort       insertion sorting algorithm for an array
+//                  validateSort        confirms that an array is actually in ascending order
+//                  freeMemory          frees up dynamically allocated memory
+//                  quickSort           quicksort sortign algorithm
+//                  findPartition       finds the partition for the quicksort algorithm
+//                  pickSort            calls the sorting algorithm the user selected
+//                  getInputHelper      reads number entered by user and validates the value
+//                  getInput            Gets the necessary input from the user
+//                  displayMainMenu     displays the main menu and program instructions to user
+//                  copyArray           copies values from one array to another equally sized array
+//                  loopControl         superprocedure controlling test cycles for sorting algorithms
+//                  bubbleSort          sorts an array using bubble sort algorithm
+//                  mergeA              merges values in mergesort algorithm
+//                  mergeSort           sorts array using merge sort algorithms
+//                  displayRunResult    Displays the result of each sort run
+//                  getSortName         provides the string name of the sort option
+//                  displayResults      Displays the statistics of the sorts chosen by user
+//**********************************************************************
 
 #include "Common.h"
 #include "tatsumotoFuncs.h"
